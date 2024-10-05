9551f7279983783031a38250b6efde4a68ff61b5
Edwin Ajong <edwinajong@gmail.com>
2024-10-05 17:13:55 -0500

    main package

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"eddybruv/albums-api/db"
+	"eddybruv/albums-api/handlers"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// load env file
+	if envErr := godotenv.Load(); envErr != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	//initialize db
+	db.InitDB()
+
+	//gin router
+	router := gin.Default()
+
+	//routes
+	router.GET("/albums", handlers.GetAlbums)
+	router.GET("/albums/:artist", handlers.GetAlbumsByArtist)
+
+	err := router.Run("localhost:8080")
+	if err != nil {
+		return
+	}
+}
