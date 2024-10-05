a49724c5dfb3da4b5ab05deaf09869aaafb5e593
Edwin Ajong <edwinajong@gmail.com>
2024-10-05 17:14:42 -0500

    added handlers/controllers

---
+++ handlers/album_handlers.go
@@ -0,0 +1,73 @@
+package handlers
+
+import (
+	"database/sql"
+	"eddybruv/albums-api/db"
+	"eddybruv/albums-api/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+func GetAlbums(c *gin.Context) {
+	var albums []models.Album
+
+	db.InitDB()
+	rows, err := db.DB.Query(`select id, title, artist, price from album`)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+	}(rows)
+
+	for rows.Next() {
+		var tempAlbum models.Album
+		err := rows.Scan(&tempAlbum.ID, &tempAlbum.Title, &tempAlbum.Artist, &tempAlbum.Price)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		albums = append(albums, tempAlbum)
+	}
+
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func GetAlbumsByArtist(c *gin.Context) {
+	artist := strings.ToLower(c.Param("artist"))
+
+	var albums []models.Album
+
+	query := "Select distinct * from album where lower(artist) like ?"
+	rows, err := db.DB.Query(query, "%"+artist+"%")
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+	}(rows)
+
+	for rows.Next() {
+		var tempAlb models.Album
+		err := rows.Scan(&tempAlb.ID, &tempAlb.Title, &tempAlb.Artist, &tempAlb.Price)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		albums = append(albums, tempAlb)
+	}
+
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+//if err := c.BindJSON(&newAlbum); err != nil {
+//return
+//}
